d8f12b25ac060cd081d4e2eb426bb7e67610b9ed
brentli1 <brent.jlinville@gmail.com>
2017-09-11 21:44:49 -0500

    add movie categories table; add movie and category relationships; update git ignore

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Movie;
 
 class Category extends Model
 {
-    //
+    public function movies() {
+        return $this->hasMany('App\Movie');
+    }
 }
--- app/Movie.php
+++ app/Movie.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Category;
 
 class Movie extends Model
 {
-    //
+    public function categories() {
+        return $this->hasMany('App\Category');
+    }
 }
