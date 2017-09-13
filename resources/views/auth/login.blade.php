146bd32ba7d4aac92e65b7b97a3782528107091d
Nick Pruitt <nick@abenity.com>
2017-09-12 21:33:44 -0500

    Login page updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,60 +2,66 @@
 
 @section('content')
 <div class="container">
-<div class="row justify-content-center p-3">
-    <div class="card col-sm-8 col-md-8">
-        <div class="card-body">
-
-            <form role="form" method="POST" action="{{ url('/login') }}">
-                {!! csrf_field() !!}
-
-                <div class="form-group">
-                    <label class="form-control-label">E-Mail Address</label>
-                    <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
-
-                    @if ($errors->has('email'))
-                        <div class="invalid-feedback">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </div>
-                    @endif
+    <div class="row justify-content-center p-3">
+        <div class="col-sm-8 col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    Login
                 </div>
 
-                <div class="form-group">
-                    <label class="form-control-label">Password</label>
-                    <input type="password" class="form-control{{ $errors->has('password') ? ' has-error' : '' }}" name="password">
+                <div class="card-body">
 
-                    @if ($errors->has('password'))
-                        <div class="invalid-feedback">
-                            <strong>{{ $errors->first('password') }}</strong>
+                    <form role="form" method="POST" action="{{ url('/login') }}">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <label class="form-control-label">E-Mail Address</label>
+                            <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
+
+                            @if ($errors->has('email'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </div>
+                            @endif
                         </div>
-                    @endif
-                </div>
 
-                <div class="form-group">
-                    <label class="custom-control custom-checkbox">
-                        <input type="checkbox" name="remember" class="custom-control-input">
-                        <span class="custom-control-indicator"></span>
-                        <span class="custom-control-description">Remember Me</span>
-                    </label>
-                </div>
+                        <div class="form-group">
+                            <label class="form-control-label">Password</label>
+                            <input type="password" class="form-control{{ $errors->has('password') ? ' has-error' : '' }}" name="password">
 
-                <div class="form-group">
-                    <div class="media">
-                        <div class="d-flex align-self-center mr-3">
-                            <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-btn fa-sign-in"></i>Login
-                            </button>
+                            @if ($errors->has('password'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </div>
+                            @endif
                         </div>
-                        <div class="media-body d-flex align-self-center">
-                            <a href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+
+                        <div class="form-group">
+                            <label class="custom-control custom-checkbox">
+                                <input type="checkbox" name="remember" class="custom-control-input">
+                                <span class="custom-control-indicator"></span>
+                                <span class="custom-control-description">Remember Me</span>
+                            </label>
                         </div>
-                    </div>
+
+                        <div class="form-group">
+                            <div class="media">
+                                <div class="d-flex align-self-center mr-3">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-btn fa-sign-in"></i>Login
+                                    </button>
+                                </div>
+                                <div class="media-body d-flex align-self-center">
+                                    <a href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
                 </div>
-
-            </form>
-
+            </div>
         </div>
     </div>
-</div>
 </div>
 @endsection
